de3489c4beb193aa99273063166a3189174a07aa
Олико Человик <kaper156oi@gmail.com>
2019-06-22 10:45:03 +0600

    Create basic site design for pages: profiles(bad), news, post. Redefine footer and header

--- SocialHouse/applications/core/views.py
+++ SocialHouse/applications/core/views.py
@@ -3,11 +3,13 @@ from django.contrib.auth.decorators import login_required
 
 
 def index(req):
-    return render(request=req, template_name="index.html")
+    return render(request=req, template_name='index.html')
 
 
 @login_required
 def profile(req):
-    return redirect(index)
-
-# Create your views here.
+    from .models import Worker, WorkerPosition
+    context = dict()
+    context['worker'] = Worker.objects.get(user__username='admin')
+    context['worker_positions'] = WorkerPosition.objects.filter(worker=context['worker'])
+    return render(request=req, template_name='core/profile.html', context=context)
