cf8faaf4aba1426377327483a21ed6c95a5606e6
kaper156 <156-oi@i.ua>
2019-08-22 17:21:07 +0300

    Header toggler fix

--- SocialHouse/applications/core/views.py
+++ SocialHouse/applications/core/views.py
@@ -8,6 +8,8 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = 'index.html'
 
+
 @method_decorator(login_required, name='dispatch')
 class ProfileView(TemplateView):
     template_name = 'core/profile.html'
+
