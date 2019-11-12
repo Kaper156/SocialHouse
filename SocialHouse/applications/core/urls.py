8f54341ce62db6c1bcfbc2176e6b55a487fc4a1b
kaper156 <156-oi@i.ua>
2019-11-12 15:30:44 +0600

    Add servicedPerson-list to profile; Add servicedPerson-create to profile;

--- SocialHouse/applications/core/urls.py
+++ SocialHouse/applications/core/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('serviced_person', views.ServicedPersonListView.as_view(), name='serviced_person_list'),
     path('serviced_person/<pk_sp>', views.ServicedPersonDetailView.as_view(), name='serviced_person_detail'),
     path('passport_data/<pk_pas>', views.PassportDataDetailView.as_view(), name='passport_data_detail'),
 ]
--- SocialHouse/applications/core/views.py
+++ SocialHouse/applications/core/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from django.views.generic import TemplateView
 
 from .models import ServicedPerson, PassportData
@@ -18,3 +18,9 @@ class PassportDataDetailView(DetailView):
     model = PassportData
     template_name = 'core/passport_data_detail.html'
     pk_url_kwarg = 'pk_pas'
+
+
+class ServicedPersonListView(ListView):
+    model = ServicedPerson
+    template_name = 'core/serviced_person_list.html'
+    paginate_by = 50
