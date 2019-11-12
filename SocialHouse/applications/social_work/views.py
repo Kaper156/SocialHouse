1568f90492b26b3ab189f35c7becff22045d8302
kaper156 <156-oi@i.ua>
2019-11-12 14:22:48 +0600

    Fix SJ multiply in createView of socialJournals

--- SocialHouse/applications/social_work/views.py
+++ SocialHouse/applications/social_work/views.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView
-from django.db.models import Q
 
 from applications.core.models import Worker
 from .forms import ServiceJournalForm
-from .utils import get_range_around_month
 from .models import ServiceJournal
+from .utils import get_range_around_month
 
 
 @method_decorator(login_required, name='dispatch')
@@ -19,8 +19,9 @@ class ServiceJournalCreateView(CreateView):
         ctx = super(ServiceJournalCreateView, self).get_context_data(**kwargs)
         worker = Worker.objects.get(user=self.request.user)
         d1, d2 = get_range_around_month()
-        # TODO fix multiply of SJ
-        ctx['objs'] = ServiceJournal.objects.filter(Q(employer__worker=worker)
-                                                    & Q(service__servicejournal__date_of__range=(d1, d2)))
+        ctx['objs'] = ServiceJournal.objects.filter(employer__worker=worker, date_of__range=(d1, d2))
         ctx['objs_cnt'] = ctx['objs'].count()
         return ctx
+
+    def get_success_url(self):
+        return reverse('url_services_journals_create')
